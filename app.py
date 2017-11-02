fd3f0f498cb4deb2ef4870ce8595d3c964c83fcf
junsuLime <vfv0121@gmail.com>
2017-11-02 20:24:06 +0900

    omit initialize and start

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from osorihack.view import refresh_information
 
 if __name__ == "__main__":
     print("Start osori hack-day")
-    app.listen(9000)
+    app.listen(80)
     print("ready for initialize data")
-    init_data_func = functools.partial(refresh_information, app.managed_info)
-    IOLoop.current().run_sync(init_data_func)
+    # init_data_func = functools.partial(refresh_information, app.managed_info)
+    # IOLoop.current().run_sync(init_data_func)
     print("initialize data is done")
     print("Start Server")
     IOLoop.current().start()
