969a9431106d94e4fa5aef91e88a5489b8f51661
JieunKim <laurelis37@gmail.com>
2017-11-02 12:47:26 +0900

    home: fixed timer error

--- static/timer.js
+++ static/timer.js
@@ -17,12 +17,21 @@ function showtime() {
     var CurMinute = now.getMinutes();
     var CurDate = now.getDate();
     var CurSecond = now.getSeconds();
-    var Hourleft = 23 - CurHour;
-    var Minuteleft = 59 - CurMinute;
-    var Secondleft = 59 - CurSecond;
-    var Dateleft = 20 - CurDate;
+    var Hourleft = 9 - CurHour;
+    var Minuteleft = 30 - CurMinute;
+    var Secondleft = 0 - CurSecond;
+    var Dateleft = 5 - CurDate;
 
     Hourleft += Dateleft * 24;
+
+    if (Secondleft < 0) {
+        Secondleft += 60;
+        --Minuteleft;
+    }
+    if (Minuteleft < 0) {
+        Minuteleft += 60;
+        --Hourleft;
+    }
 
     var timer = document.getElementById('timer');
     deadline = new Date(2017,11,5,9,30,0);
