7fc3782e5e84c3c7a40e526f6ede90629dec6bae
junsuLime <vfv0121@gmail.com>
2017-11-02 22:08:54 +0900

    host = '0.0.0.0'

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from osorihack.view import refresh_information
 if __name__ == "__main__":
     print("Start osori hack-day")
     http_server = HTTPServer(app)
-    http_server.listen(5000)
+    http_server.listen(5000, "0.0.0.0")
     print("ready for initialize data")
     # init_data_func = functools.partial(refresh_information, app.managed_info)
     # IOLoop.current().run_sync(init_data_func)
