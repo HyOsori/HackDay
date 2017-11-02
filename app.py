b4affbbe5ffec4a783454a7cbdcafd509d205a4e
junsuLime <vfv0121@gmail.com>
2017-11-02 20:29:50 +0900

    convert port 80 -> 9000

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from osorihack.view import refresh_information
 
 if __name__ == "__main__":
     print("Start osori hack-day")
-    app.listen(80)
+    app.listen(9000)
     print("ready for initialize data")
     # init_data_func = functools.partial(refresh_information, app.managed_info)
     # IOLoop.current().run_sync(init_data_func)
