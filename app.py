c8f1d79d9179f1007eb355f392512dfa105b19af
junsuLime <vfv0121@gmail.com>
2017-11-02 20:37:25 +0900

    run this with http server

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import functools
-
+from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from osorihack import app
 from osorihack.view import refresh_information
 
 if __name__ == "__main__":
     print("Start osori hack-day")
-    app.listen(9000)
+    http_server = HTTPServer(app)
+    http_server.listen(5000)
     print("ready for initialize data")
     # init_data_func = functools.partial(refresh_information, app.managed_info)
     # IOLoop.current().run_sync(init_data_func)
