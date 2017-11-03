cbfc841ef70c38a6e207e54953668f835c7d77c6
junsuLime <vfv0121@gmail.com>
2017-11-03 21:31:59 +0900

    add logs on heroku

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ define("port", default=5000, help="run on the given port", type=int)
 def run():
     parse_command_line()
 
+    print("initialize cache data")
     cache_initialize_func = functools.partial(refresh_information, app.managed_info)
     IOLoop.instance().run_sync(cache_initialize_func)
 
+    print("start server")
     app.listen(options.port)
     IOLoop.instance().start()
 
